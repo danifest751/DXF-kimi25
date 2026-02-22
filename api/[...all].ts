739dde663ed1493cd00dd867738be9145060c393
danifest751 <danifest751@gmail.com>
2026-02-22 19:42:53 +0500

    fix(vercel): disable body parser for api catch-all

--- api/[...all].ts
+++ api/[...all].ts
@@ -1,5 +1,12 @@
 import app from '../packages/api-service/src/index.js';
 
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
+
 export default function handler(req: any, res: any): void {
   app(req, res);
 }
